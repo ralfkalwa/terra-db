3fce546e9841d9cc951dd1fba5c812307804e4d5
Ralf Kalwa <ralf_kalwa@t-online.de>
2024-02-24 20:10:44 +0100

    check routine for sql script

---
+++ checkdb.py
@@ -0,0 +1,56 @@
+import sqlite3
+import pandas as pd
+
+# for file handling
+import os
+
+CONNECTION_OBJECT = "terra2023.db"
+SQL_FILE = "terra2023.sql"
+
+
+def run_query(q):
+    """
+    # function to return a pandas dataframe from a query
+    """
+    try:
+        with sqlite3.connect(CONNECTION_OBJECT) as conn:
+            # cur = conn.cursor()   #Obtain a cursor object
+            qr = pd.read_sql_query(q, conn)
+    except sqlite3.Error as ex:
+        print(ex)
+    return qr
+
+
+def run_command(c) -> None:
+    """
+    # function to run sqlite command
+    """
+    try:
+        with sqlite3.connect(CONNECTION_OBJECT) as conn:
+            # tells sqlite to autocommit any changes
+            conn.isolation_level = None  
+            conn.execute(c)
+    except sqlite3.Error as ex:
+        print(ex)
+
+
+# clear previous run
+try:
+    os.remove(CONNECTION_OBJECT)
+except os.error as ex_remove:
+    print(ex_remove)
+
+# Read the contents of your .sql file
+with open(SQL_FILE, "r", encoding="UTF-8") as input_file:
+    sql_script = input_file.read()
+
+# Connect to your SQLite database
+db = sqlite3.connect(CONNECTION_OBJECT)
+cursor = db.cursor()
+
+# Execute the SQL script
+cursor.executescript(sql_script)
+
+# Commit changes and close the connection
+db.commit()
+db.close()
